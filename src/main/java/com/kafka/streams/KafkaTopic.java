b9757a8ef2e6265aeeb58cede07ab468a28186e6
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-16 22:04:21 +0900

    feat: 카프카 스트림즈 - 행 분리 예제 추가

--- src/main/java/com/kafka/streams/KafkaTopic.java
+++ src/main/java/com/kafka/streams/KafkaTopic.java
@@ -3,4 +3,5 @@ package com.kafka.streams;
 public abstract class KafkaTopic {
     public static final String STREAMS_PLAINTEXT_INPUT = "streams-plaintext-input";
     public static final String STREAMS_PIPE_OUTPUT = "streams-pipe-output";
+    public static final String STREAMS_LINE_SPLIT_OUTPUT = "streams-line-split-output";
 }
---
+++ src/main/java/com/kafka/streams/LineSplit.java
@@ -0,0 +1,49 @@
+package com.kafka.streams;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ValueMapper;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class LineSplit {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, StreamsApplicationId.STREAMS_LINE_SPLIT);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, StreamsConfigValue.BOOTSTRAP_SERVERS);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        // 토폴로지 만들기
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream(KafkaTopic.STREAMS_PLAINTEXT_INPUT);
+
+        // flatMap, flatMapValues 메소드는 이전 스트림의 상태를 참고하지 않는다. -> '무상태 오퍼레이터' 라고도 한다.
+        source.flatMapValues((ValueMapper<String, Iterable<String>>) value -> Arrays.asList(value.split("\\W+")))
+            .to(KafkaTopic.STREAMS_LINE_SPLIT_OUTPUT);
+
+        // 토폴로지를 만든다.
+        final Topology topology = builder.build();
+
+        System.out.println(topology.describe());
+
+        // 카프카 스트림즈 생성 및 실행
+        try (KafkaStreams streams = new KafkaStreams(topology, props)) {
+            final CountDownLatch latch = new CountDownLatch(1);
+            streams.start();
+            System.out.println("topology started");
+            latch.await();
+        } catch (Throwable throwable) {
+            System.exit(1);
+        }
+
+        System.exit(0);
+    }
+}
